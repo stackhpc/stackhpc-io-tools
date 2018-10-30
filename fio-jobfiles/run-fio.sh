248c7376d2693507c3c5c760469e583784b7037d
Bharat Kunwar <b.kunwar@gmail.com>
2018-10-30 12:31:20 +0000

    Simplify run script

--- fio-jobfiles/run-fio.sh
+++ fio-jobfiles/run-fio.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
-result_dir=/data/read-random/${JOB_NAME}
-mkdir -p $result_dir
+export WORK_DIR=/data/read-random/$JOB_NAME
+mkdir -p $WORK_DIR
 let i=128
 let lim=16*1024*1024
 while [ $i -le $lim ]
 do
     echo $i
-    fio fio-jobfiles/read-random.fio --runtime=30 --output-format=json+ --blocksize=$i > $result_dir/$i.json
+    fio fio-jobfiles/read-random.fio --runtime=30 --output-format=json+ --blocksize=$i > $WORK_DIR/$i.json
     let i=2*i
 done
 
-rm  /data/${JOB_NAME}-*.dat
+rm $WORK_DIR/*.dat
