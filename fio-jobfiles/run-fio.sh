c6db9a5babd3509ef2b8c502a24b60e5029f3142
Bharat Kunwar <b.kunwar@gmail.com>
2018-10-30 12:18:37 +0000

    Clean up files after fio job

--- fio-jobfiles/run-fio.sh
+++ fio-jobfiles/run-fio.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-result_dir=/data/fio-$(date '+%F-%T')
-
-read_random_result_dir=$result_dir/read-random
-mkdir -p $read_random_result_dir
-i=128
+result_dir=/data/read-random/${JOB_NAME}
+mkdir -p $result_dir
+let i=128
 let lim=16*1024*1024
 while [ $i -le $lim ]
 do
     echo $i
-    fio fio-jobfiles/read-random.fio --runtime=30 --output-format=json+ --blocksize=$i > $read_random_result_dir/read-random-$i.json
+    fio fio-jobfiles/read-random.fio --runtime=30 --output-format=json+ --blocksize=$i > $result_dir/$i.json
     let i=2*i
 done
 
+rm  /data/${JOB_NAME}-*.dat
