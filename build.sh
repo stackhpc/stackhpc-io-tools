d742dbaf27218786f880067520a4fc2363a673bc
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-01 14:00:22 +0000

    Update stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #! /bin/env bash
 
 export FIO_VERSION=3.11
+export FIO_TAG=v${FIO_VERSION}.10
 
-docker build . --build-arg FIO_VERSION=${FIO_VERSION} \
-    -t brtknr/fio:v${FIO_VERSION} && \
-    docker push brtknr/fio:v${FIO_VERSION}
+sudo docker build . --build-arg FIO_VERSION=${FIO_VERSION} \
+    -t brtknr/fio:${FIO_TAG} && \
+    sudo docker push brtknr/fio:${FIO_TAG} && \
+    sed -i 's/fio:v.*$/fio:'${FIO_TAG}'/g' k8s/jobs.yaml && \
+    kubectl delete -f k8s/jobs.yaml || \
+    kubectl apply -f k8s/jobs.yaml
