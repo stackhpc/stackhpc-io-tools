057dfbabec4e94c69db0e0a43d9a6849f06d7403
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-01 14:05:01 +0000

    kubectl executions independent of docker build

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ export FIO_TAG=v${FIO_VERSION}.10
 sudo docker build . --build-arg FIO_VERSION=${FIO_VERSION} \
     -t brtknr/fio:${FIO_TAG} && \
     sudo docker push brtknr/fio:${FIO_TAG} && \
-    sed -i 's/fio:v.*$/fio:'${FIO_TAG}'/g' k8s/jobs.yaml && \
-    kubectl delete -f k8s/jobs.yaml || \
-    kubectl apply -f k8s/jobs.yaml
+    sed -i 's/fio:v.*$/fio:'${FIO_TAG}'/g' k8s/jobs.yaml
+kubectl delete -f k8s/jobs.yaml
+kubectl apply -f k8s/jobs.yaml
