41368a0ba40930bec5ade903a7dc028c6d81841d
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-02 18:41:23 +0000

    Alpine jobs weren't completing so use CentOS image

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #! /bin/env bash
 
-export FIO_VERSION=3.11
-export FIO_TAG=v${FIO_VERSION}.11
+export FIO_VERSION=3.1
+export FIO_TAG=v${FIO_VERSION}
 export K8S_JOBSPEC=k8s/beegfs-jobs.yml
+
+kubectl delete -f ${K8S_JOBSPEC}
 
 sudo docker build . --build-arg FIO_VERSION=${FIO_VERSION} \
     -t brtknr/fio:${FIO_TAG} && \
     sudo docker push brtknr/fio:${FIO_TAG} && \
-    sed -i 's/fio:v.*$/fio:'${FIO_TAG}'/g' ${K8S_JOBSPEC}
-kubectl delete -f ${K8S_JOBSPEC}
-kubectl apply -f ${K8S_JOBSPEC}
+    sed -i 's/fio:v.*$/fio:'${FIO_TAG}'/g' ${K8S_JOBSPEC} && \
+    kubectl apply -f ${K8S_JOBSPEC}
--- fio_jobfiles/run_fio.sh
+++ fio_jobfiles/run_fio.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export DATA_DIR=/data/fio_datafiles/k8s
-export WORK_DIR=/data/$JOB_NAME/$POD_NAME
+set -ux
+export DATA_DIR=/data/$SUB_PATH/fio_datafiles/k8s
+export WORK_DIR=/data/$SUB_PATH/$JOB_NAME/$POD_NAME
 mkdir -p $DATA_DIR
 mkdir -p $WORK_DIR
 let i=128
@@ -8,6 +9,6 @@ let lim=16*1024*1024
 while [ $i -le $lim ]
 do
     echo $i
-    fio /fio_jobfiles/read_random.fio --runtime=30 --directory=$DATA_DIR --output-format=json+ --blocksize=$i > $WORK_DIR/$i.json
+    fio /fio_jobfiles/read_random.fio --runtime=30 --directory=$DATA_DIR --output-format=json+ --blocksize=$i --output=$WORK_DIR/$i.json
     let i=2*i
 done
