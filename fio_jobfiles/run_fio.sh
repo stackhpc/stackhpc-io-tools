75cc11b40c1f10d9c918a5ca48938242f9bd6c37
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-09 17:47:30 +0000

    BeeGFS read and write jobs with locks to ensure syncronised jobs

--- fio_jobfiles/run_fio.sh
+++ fio_jobfiles/run_fio.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 set -ux
-export DATA_DIR=/data/$SUB_PATH/fio_datafiles/k8s
-export WORK_DIR=/data/$SUB_PATH/$JOB_NAME/$POD_NAME
+
+if [ "$FIO_JOB" = "write" ]
+then export DATA_DIR=/data/fio_write/$POD_NAME
+else export DATA_DIR=/data/fio_read; fi
 mkdir -p $DATA_DIR
-mkdir -p $WORK_DIR
-let i=128
-let lim=16*1024*1024
-while [ $i -le $lim ]
-do
-    echo $i
-    fio /fio_jobfiles/read_random.fio --runtime=30 --directory=$DATA_DIR --output-format=json+ --blocksize=$i --output=$WORK_DIR/$i.json
-    let i=2*i
+export JOB_DIR=/data/$JOB_NAME
+export RESULT_DIR=$JOB_DIR/$POD_NAME
+mkdir -p $RESULT_DIR
+echo $POD_NAME >> $JOB_DIR/0.lock
+while [ $(cat $JOB_DIR/0.lock | wc -l) -le $NUM_PODS ]; do usleep 1000; done
+let i=128; let lim=16*1024*1024
+while [ $i -le $lim ]; do
+  echo $i
+  fio /fio_jobfiles/global_config.fio --fallocate=none --runtime=30 --directory=$DATA_DIR --output-format=json+ --blocksize=$i --output=$RESULT_DIR/$i.json
+  echo $POD_NAME >> $JOB_DIR/$i.lock
+  while [ $(cat $JOB_DIR/$i.lock | wc -l) -le $NUM_PODS ]; do usleep 1000; done
+  let i=2*i
 done
+rm $JOB_DIR/*.lock
+if [ "$FIO_JOB" = "write" ]; then rm -rf $DATA_DIR; fi
+chown -R ${RESULT_USER:-1000}:${RESULT_GROUP:-1000} $RESULT_DIR
