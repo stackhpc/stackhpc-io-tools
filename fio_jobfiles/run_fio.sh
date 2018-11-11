9db7acafa960cf11c87d0228dbe36d2a9c16bc0a
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-11 09:41:01 +0000

    Make run_fio.sh tolerant to ceph asynchronous latencies

--- fio_jobfiles/run_fio.sh
+++ fio_jobfiles/run_fio.sh
@@ -10,17 +10,20 @@ fi
 export JOB_DIR=/data/$JOB_NAME
 mkdir -p $JOB_DIR
 export JOB_LOCK="${JOB_DIR}/${FIO_JOB}.lock"
-if [ ! -f $JOB_LOCK ]; then rm -rf $JOB_DIR; touch $JOB_LOCK; fi
+if [ ! -f "$JOB_LOCK" ]; then
+  for f in $(ls -I "${JOB_DIR}/*.lock"); do rm -rf $JOB_DIR/$f; done
+  touch $JOB_LOCK
+fi
 export RESULT_DIR=$JOB_DIR/$POD_NAME
 mkdir -p $RESULT_DIR
-let bs=0; BS_LOCK=${JOB_DIR}/${bs}.lock; mkdir -p $BS_LOCK; touch $BS_LOCK/$POD_NAME
-while [ $(ls $BS_LOCK | wc -l) -lt $NUM_PODS ]; do usleep 1000; done
+let bs=0; BS_LOCK=${JOB_DIR}/${bs}.lock; mkdir -p $BS_LOCK
+while [ $(ls $BS_LOCK | wc -l) -lt $NUM_PODS ]; do touch $BS_LOCK/$POD_NAME; usleep 100000; done
 let bs=128; let lim=16*1024*1024
 while [ $bs -le $lim ]; do
   echo $bs
   fio /fio_jobfiles/global_config.fio --fallocate=none --runtime=30 --directory=$SCRATCH_DIR --output-format=json+ --blocksize=$bs --output=$RESULT_DIR/$bs.json
   BS_LOCK=${JOB_DIR}/${bs}.lock; mkdir -p $BS_LOCK; touch $BS_LOCK/$POD_NAME
-  while [ $(ls $BS_LOCK | wc -l) -lt $NUM_PODS ]; do usleep 1000; done
+  while [ $(ls $BS_LOCK | wc -l) -lt $NUM_PODS ]; do touch $BS_LOCK/$POD_NAME; usleep 100000; done
   let bs=2*bs
 done
 rm -rf $JOB_DIR/*.lock
