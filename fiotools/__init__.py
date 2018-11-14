93f7b9a18691a1269a29b26850db339b7f1b6f50
Bharat Kunwar <b.kunwar@gmail.com>
2018-11-14 17:41:24 +0000

    Move plotters from notebook to parser, use pandas for aggregating data

--- fiotools/__init__.py
+++ fiotools/__init__.py
@@ -1,6 +1,7 @@
 # .config/matplotlib/matplotlibrc line backend : Agg
 import matplotlib.pyplot as plt
 from pathlib2 import Path
+import pandas as pd
 import numpy as np
 import errno
 import json
@@ -24,8 +25,9 @@ class ClatGrid:
         'us': {'divider': 10**3, 'label':'\mu s'},
         'ms': {'divider': 10**6, 'label':'m s'},
     }
+    percentiles = ['1%','5%','10%','20%','30%','40%','50%','60%','70%','80%','90%','95%','99%','99.5%','99.9%','99.95%','99.99%']
     
-    def __init__( self, input_dirs, output_dir, granularity, force, mode, skip_bs=[], logscale=False, timescale='us', max_bs=65536):
+    def __init__(self, input_dirs, output_dir, granularity, force, mode, skip_bs=[], logscale=False, timescale='us', max_bs=65536, verbose=False, plot=True):
         self.grid_y = granularity
         self.logscale = logscale
         self.timescale = timescale
@@ -36,13 +38,15 @@ class ClatGrid:
         self.input_dirs = input_dirs
         self.output_dir = output_dir
         self.mode = mode
-        
+        self.verbose = verbose
         ensure_output_dir(output_dir, force)
-        for input_dir in input_dirs:
-            print "Scanning for fio data in %s" % input_dir
-            self.populate(input_dir, output_dir)
+        self.populate(input_dirs, output_dir)
         self.aggregate_and_normalise()
-
+        if plot:
+            self.plot_cl()
+            self.plot_il()
+            self.plot_bw()
+        
     # Each series is indexed by the IO size (and the test mode)
     # Multiple client series are stored independently at this stage
     # and will be gridded, aggregated and normalised later on.
@@ -50,8 +54,7 @@ class ClatGrid:
 
         # Paranoia: Check the iops_total matches the sum of all bins
         if sum(clat_data.values()) != iops_total:
-            print "I/O size %d: sum of histogram bins is %d, expected %d" % (bs, sum(clat_data.values()), iops_total)
-            raise ValueError
+            raise ValueError("I/O size %d: sum of histogram bins is %d, expected %d" % (bs, sum(clat_data.values()), iops_total))
 
         # Construct a dict of floating-point IO latencies
         bs_data = {}
@@ -77,7 +80,7 @@ class ClatGrid:
         self.io_bs[x] += [bs_data]
         self.iops_bs[x] += iops_total
 
-
+        
     def aggregate_and_normalise( self ):
         ''' We may have sampled multiple results per blocksize.
             Generate a weighted normalisation across all readings.
@@ -153,18 +156,34 @@ class ClatGrid:
                 io_density_check += D['density']*(D['upper']-D['lower'])
 
             # Paranoia
-            if io_density_check < 0.999 or io_density_check > 1.001 or grid_check < 0.999 or grid_check > 1.001:
-                print "CHECK FAILED: blocksize %d cumulative density %f cumulative grid %f" % (2**x, io_density_check, grid_check)
-                raise ValueError
+            if abs(io_density_check - 0.99) > 0.2 or abs(grid_check - 0.99) > 0.2:
+                raise ValueError("CHECK FAILED: blocksize %d cumulative density %f cumulative grid %f" % (2**x, io_density_check, grid_check))
 
         return grid
 
+    def plot_cl(self):
+        fig, ax = plt.subplots(figsize=(10,8))
+        legend = sorted(set(self.ildf.index))
+        ax.set_prop_cycle('color', [plt.cm.jet(i) for i in np.linspace(0, 1, len(legend))])
+        self.cldf.groupby(['log2_bs','clat'])['freq'].mean().reset_index().set_index('log2_bs').groupby('log2_bs').plot(x='clat',y='freq',ax=ax,loglog=True,linewidth=1)
+        ax.legend(legend, title='block size ($2^n$)')
+        ax.set_xlabel('commit latency - ($ns$)')
+        ax.set_ylabel('mean frequency')
+        plt.savefig(str(self.output_dir/'commit-latency-freq-dist.png'))
+        return fig, ax
 
-    # OK we have enough data, construct the grid and populate with interpolations
-    def plot_data(self, output_dir, filename='blob.png',cmap='copper',colorbar=False):
+    def plot_bw(self):
+        fig, ax = plt.subplots(figsize=(10,8))
+        pd.concat([pd.Series(row, name=i) for i,row in self.bwdf['bw']
+                   .groupby(self.bwdf.index).apply(list).iteritems()], axis=1).boxplot(ax = ax)
+        ax.set_xlabel('block size - $2^n$')
+        ax.set_ylabel('%s bandwidth ($KB/s$)' % self.mode)
+        plt.savefig(str(self.output_dir/'blocksize-vs-bandwidth.png'))      
+        return fig, ax
+    
+    def plot_il(self, percentiles=[50.0,95.0,99.0,99.99], xlim=None, ylim=None, cmap='gist_heat'):
+        fig, ax = plt.subplots(figsize=(10,8))
         grid = self.fit_to_grid()
-        
-        fig = plt.figure()
         # Find maximum value on Grid
         max_z = grid.max()
         nrow, ncol = grid.shape
@@ -175,85 +194,81 @@ class ClatGrid:
                 if grid[row, col] == 0.0:
                     grid[row, col] = np.nan
 
+        if xlim == None:
+            xlim = [self.min_x,self.max_x]
+        if ylim == None:
+            ylim = [self.min_y,self.max_y]
         extent = (self.min_x-0.5, self.max_x+0.5, self.min_y, self.max_y)
         plt.imshow(grid, extent=extent, cmap=cmap, origin='lower', vmin=0.0, vmax=max_z, aspect='auto', interpolation='none')
+        self.ildf[percentiles].apply(lambda x: np.log10(x/self.divider)) \
+            .groupby(self.ildf.index).mean() \
+            .plot(figsize=(10,8), ax=ax, xlim=xlim, ylim=ylim, linewidth=2, style='-')
+        ax.legend(title='percentiles')
+        ax.set_xlabel('block size - $2^n$')
         if self.logscale:
-            #plt.ylim(5,7)
-            plt.ylabel('log (commit latency) - $%s$' % self.label)
+            ax.set_ylabel('log(commit latency) - $%s$' % self.label)
         else:
-            #plt.ylim(10**5,10**7)
-            plt.ylabel('commit latency - $%s$' % self.label)
-        plt.xlabel(r'block size - $2^n$')
-        if colorbar:
-            plt.colorbar(label='relative frequency per blocksize')
-        filename = "%s/%s" % (output_dir, filename)
-        plt.savefig(filename, dpi=150, orientation='landscape', transparent=False)
-        print 'Plotting to %s' % filename
-        return fig
-
-    def populate(self, input_dir, output_dir):
+            ax.set_ylabel('iops latency - $%s$' % self.label)
+        plt.savefig(str(self.output_dir/'blocksize-vs-commit-latency.png'))        
+        return fig, ax
+    
+    def populate(self, input_dirs, output_dir):
         # For each blocksize found, emit data from each listed job
         # FIXME: need to incorporate hostname and dataset name in the results
         # Emit bandwidth data points in column format
-        fio_file_list = get_fio_file_list(input_dir)
-        fio_results = get_fio_results(fio_file_list)
-        bs_list = sorted(fio_results.keys())
-        for bs in bs_list:
-            for bs_job in fio_results[bs]['jobs']:
 
-                # Read and write bandwidth as a function of I/O size
-                fpath = output_dir/(self.mode+'-bandwidth.dat')
-                with fpath.open('a+') as job_fd:
-                    job_fd.write(u'{0:8}\t{1:8}\t{2:8}\n'
-                        .format(bs, bs_job[self.mode]['bw'], bs_job['write']['bw']))
+        bw = list()
+        il = list()
+        cl = list()
 
-                # IOPS and IO latency percentiles as a function of I/O size
-                fpath = output_dir/(self.mode+'-iops-latency.dat')
-                with fpath.open('a+') as job_fd:
-                    job_fd.write(u'{:8}\t{:8}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'
-                        .format(bs, bs_job[self.mode]['iops'],
-                            bs_job[self.mode]['clat_ns']['percentile']["1.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["5.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["10.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["20.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["30.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["40.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["50.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["60.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["70.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["80.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["90.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["95.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["99.000000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["99.500000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["99.900000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["99.950000"],
-                            bs_job[self.mode]['clat_ns']['percentile']["99.990000"]))
+        for input_dir in input_dirs:
+            print "Scanning for fio data in %s" % input_dir
+            fio_file_list = get_fio_file_list(input_dir)
+            fio_results = get_fio_results(fio_file_list)
+            bs_list = sorted(fio_results.keys())
+            for bs in bs_list:
+                if bs not in self.skip_bs and bs <= self.max_bs:
+                    for bs_job in fio_results[bs]['jobs']:
 
-                # Write I/O completion latencies as a datafile of x y z datapoints
-                fpath = output_dir/(self.mode+'-clat.dat')
-                with fpath.open('a+') as job_fd:
-                    # Need to transform string keys into integer to sort
-                    for bin_ns in sorted([int(x) for x in bs_job[self.mode]['clat_ns']['bins'].keys()]):
-                        bin_freq = bs_job[self.mode]['clat_ns']['bins'][str(bin_ns)]
-                        job_fd.write(u'{:8}\t{:10}\t{:8}\n'.format(math.log(bs,2), bin_ns, bin_freq))
-                    job_fd.write(u'\n')
+                        log2_bs = int(math.log(bs,2))
 
-                # Aggregate data from each dataset
-                if bs <= self.max_bs and bs not in self.skip_bs:
-                    self.add_series( int(bs), bs_job[self.mode]['total_ios'], bs_job[self.mode]['clat_ns']['bins'] ) 
+                        # Read and write bandwidth as a function of I/O size
+                        bw.append({'log2_bs': log2_bs, 'bw': bs_job[self.mode]['bw']})
 
-                print "I/O size %8d, job %s: %d samples" % (bs, self.mode, bs_job[self.mode]['total_ios'])
-            
-        print "Aggregated data for %d I/Os, max latency %f %s" % (sum(self.iops_bs.values()), self.max_y if not self.logscale else 10**self.max_y, self.timescale)
+                        # IOPS and IO latency percentiles as a function of I/O size
+                        row =  {'log2_bs': log2_bs, 'iops': bs_job[self.mode]['iops']}
+                        row.update({float(k): v for k, v in bs_job[self.mode]['clat_ns']['percentile'].iteritems()})
+                        il.append(row)
 
+                        # Write I/O completion latencies as a datafile of x y z datapoints
+
+                        # Need to transform string keys into integer to sort
+                        for bin_ns in sorted([int(x) for x in bs_job[self.mode]['clat_ns']['bins'].keys()]):
+                            bin_freq = bs_job[self.mode]['clat_ns']['bins'][str(bin_ns)]
+                            cl.append({'log2_bs': log2_bs, 'clat': bin_ns, 'freq': bin_freq})
+
+                        # Aggregate data from each dataset
+                        self.add_series( bs, bs_job[self.mode]['total_ios'], bs_job[self.mode]['clat_ns']['bins'] ) 
+                        
+                        if self.verbose:
+                            print "I/O size %8d, job %s: %d samples" % (bs, self.mode, bs_job[self.mode]['total_ios'])
+                if self.verbose:
+                    print "Aggregated data for %d I/Os, max latency %f %s" % (sum(self.iops_bs.values()), self.max_y if not self.logscale else 10**self.max_y, self.timescale)
+
+        self.cldf = pd.DataFrame(cl).set_index('log2_bs')
+        self.ildf = pd.DataFrame(il).set_index('log2_bs')
+        self.bwdf = pd.DataFrame(bw).set_index('log2_bs')
+        self.bwdf.to_csv(output_dir/(self.mode+'-bandwidth.csv'))
+        self.ildf.to_csv(output_dir/(self.mode+'-iops-latency.csv'))
+        self.cldf.to_csv(output_dir/(self.mode+'-commit-latency.csv'))
+        
 def get_fio_file_list(input_dir):
     # List JSON files in the fio input directory
     try:
         return list(input_dir.iterdir())
     except OSError as E:
-        print "Could not access input directory %s: %s" % (input_dir, os.strerror(E.errno))
-        os.abort()
+        print "Could not access input directory %s" % (input_dir)
+        raise E
 
 def ensure_output_dir(output_dir, force):
     # Check the status of the output directory
